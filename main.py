ab357a12876b9f41ce2e9626d7992ca95285aa76
Vany Eduardo Galvan <vanyegv@gmail.com>
2022-11-24 20:44:04 -0600

    import a csv

--- main.py
+++ main.py
@@ -1,38 +1,54 @@
 import pandas as pd
 from chart import generate_charts as generate_charts
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
-from fastapi.responses import FileResponse
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import HTMLResponse, FileResponse
+
 
 app = FastAPI()
 
 @app.get("/api")
+async def create_file(file: bytes = File()):
+    return {"file_size": len(file)}
+
+
+@app.get("/api2")
+async def create_upload_file(file: UploadFile):
+    return {"filename": file.filename}
+
+
+
+
+
+@app.get("/api3")
 def assign_variables():
   data = pd.read_csv('./datasets/data.csv')
   titles = list(data.head(0))
-  menu = ''
-  
+  head_options = ''
+
   for title in titles:
-    menu += f'<option value="{title}">{title}</option>'
+    head_options += f'<option value="{title}">{title}</option>'
 
   html_content = f'''
   <html>
     <body>
-      <p>Select a column</p>
+      <p>CSV to Chart</p>
         <form action='/chart'>
 
+        <label for="csv_file">Choose a csv file:</label><br>
+        <input type="file" id="csv_file" name="csv_file"><br><br>
+
         <label for="column1">Identifier</label><br>
-        <select name="column1_"><br>
-          {menu}
+        <select name="head_selection_1"><br>
+          {head_options}
         </select><br><br>
         
-        <label for="column2_">Values:</label><br>
-        <select name="column2_"><br>
-          {menu}
+        <label for="head_selection_2">Values:</label><br>
+        <select name="head_selection_2"><br>
+          {head_options}
         </select><br><br>
 
         <label for="type_chart">Type Chart:</label><br>
-        <select name="type_chart_"><br>
+        <select name="chart_selection"><br>
           <option value="bar chart">bar chart</option><br>
           <option value="pie chart">pie chart</option><br>
         </select><br><br>
@@ -45,12 +61,12 @@ def assign_variables():
   return HTMLResponse(content=html_content,status_code=200)
 
 @app.get("/chart")
-async def return_chart(column1_,column2_,type_chart_):
+async def return_chart(head_selection_1,head_selection_2,chart_selection):
   data = pd.read_csv('./datasets/data.csv')
   titles = list(data.head(0))
-  column1 = column1_ 
-  column2 = column2_ 
-  type_chart = type_chart_
+  column1 = head_selection_1 
+  column2 = head_selection_2 
+  type_chart = chart_selection
   
   if (column1 in titles) and (column2 in titles) and ('pie' in type_chart or 'bar' in type_chart):
     value1 = list(data[column1])
